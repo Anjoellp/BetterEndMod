0a8f58edac3da8e1aab2a7d148527dfe754fe4f9
anjo <anjoellp@gmail.com>
2020-09-13 10:47:10 +0200

    fixes

--- src/main/java/org/ajdp/betterend/biomes/EndIslandTransformer.java
+++ src/main/java/org/ajdp/betterend/biomes/EndIslandTransformer.java
@@ -1,6 +1,8 @@
 package org.ajdp.betterend.biomes;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.INoiseRandom;
@@ -11,26 +13,24 @@ public class EndIslandTransformer implements IAreaTransformer0 {
 	@Override
 	public int apply(INoiseRandom context, int x, int z) {
 		List<Biome> max = getHighestBiomeList();
+		System.out.println(max);
 		if (max == null)
 			return 0;
 		for (Biome b : max) {
 			int weight = CustomEndBiomeProvider.BIOME_WEIGHTS.get(b);
-			if (context.random(weight) == 0)
+			if (context.random(weight) == 0) {
+				System.out.println(b.getRegistryName() + " " + weight);
 				return max.indexOf(b);
+			}
 		}
 		return 0;
 	}
 
 	private List<Biome> getHighestBiomeList() {
-		int max = -1;
-		List<Biome> ret = null;
-		for (List<Biome> replacement : ModBiomes.replacements.values()) {
-			if (replacement.size() >= max) {
-				max = replacement.size();
-				ret = replacement;
-			}
-		}
-		return ret;
+		return ModBiomes.replacements.values().stream().reduce(new ArrayList<>(), (a, b) -> {
+			a.addAll(b);
+			return a;
+		}).stream().distinct().collect(Collectors.toList());
 	}
 
 }
